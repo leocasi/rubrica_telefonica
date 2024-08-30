1a0892eec4f817871051430d5110fd8ce3a145af
leocasi <lcasilli03@gmail.com>
2024-08-30 17:19:34 +0200

    Eseguito controllo input e colore errori/warning

--- progetto_rubrica/utility.c
+++ progetto_rubrica/utility.c
@@ -6,6 +6,57 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
+
+#define MIN_USERNAME_LENGTH 3
+#define MAX_USERNAME_LENGTH 20
+#define MIN_PASSWORD_LENGTH 8
+#define MAX_PASSWORD_LENGTH 30
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+
+void print_error(const char* message) {
+    fprintf(stderr, ANSI_COLOR_RED "%s" ANSI_COLOR_RESET "\n", message);
+  }
+
+void print_warning(const char* message){
+  fprintf(stdout, ANSI_COLOR_YELLOW "%s" ANSI_COLOR_RESET "\n",message);
+}
+
+void print_success(const char* message){
+  fprintf(stdout, ANSI_COLOR_GREEN "%s" ANSI_COLOR_RESET "\n",message);
+}
+
+int validate_username(char *username){
+  int len=strlen(username);
+  if(len<MIN_USERNAME_LENGTH||len>MAX_USERNAME_LENGTH){
+    return 0;
+  }
+  for(int i=0;i<len;i++){
+    if(!isalnum(username[i]) && username[i]!='_'){
+      return 0;
+      }
+    }
+    return 1;
+  }
+
+int validate_password(char *password){
+  int len=strlen(password);
+  if(len<MIN_PASSWORD_LENGTH||len>MAX_PASSWORD_LENGTH){
+    return 0;
+  }
+  int maiuscola=0,minuscola=0,cifra=0,carattere=0;
+  for(int i=0;i<len;i++){
+    if(isupper(password[i])) maiuscola=1;
+    else if(islower(password[i])) minuscola=1;
+    else if(isdigit(password[i])) cifra=1;
+    else carattere=1;
+  }
+  return maiuscola && minuscola && cifra && carattere;
+}
 
 
 void clear_buffers(char *buffer,char *command){
@@ -32,15 +83,28 @@ void gui_1(){
 
 void main_menu(int sfd,char *buffer,char *command,int is_authenticated){
   gui_1();
+
   while(1){
     clear_buffers(buffer,command);
     int choice=0;
-    scanf("%d",&choice);
-    getchar();
+    char line[256];
+    if(fgets(line,sizeof(line),stdin)!=NULL){
+      line[strcspn(line,"\n")]=0;
+      if(strlen(line)==1 && isdigit(line[0])){
+        choice=line[0]-'0';
+      }
+      else {
+      print_error("input non valido\n");
+      clear_buffers(buffer,command);
+      memset(line, 0, sizeof(line));
+      main_menu(sfd,buffer,command,is_authenticated);
+      }
+    }
+
     switch(choice){
     case 1:
           if(is_authenticated==0){
-            printf("You must be logged-in\n");
+            print_error("You must be logged-in\n");
             clear_buffers(buffer,command);
             main_menu(sfd,buffer,command,is_authenticated);
           }
@@ -49,10 +113,22 @@ void main_menu(int sfd,char *buffer,char *command,int is_authenticated){
           printf("Inserisci nome: ");
           fgets(nome_contatto, sizeof(nome_contatto), stdin);
           nome_contatto[strcspn(nome_contatto, "\n")] = 0;
+          if(validate_username(nome_contatto)==0){
+            print_error("Nome non valido, può contenere solo caratteri alfanumerici e underscore");
+            clear_buffers(buffer,command);
+            main_menu(sfd,buffer,command,is_authenticated);
+          }
           char numero_contatto[50];
           printf("Inserisci numero: ");
           fgets(numero_contatto,sizeof(numero_contatto),stdin);
           numero_contatto[strcspn(numero_contatto,"\n")]=0;
+          for(int i=0;i<sizeof(numero_contatto);i++){
+            if(!isdigit(numero_contatto[i])){
+              print_error("numero non valido");
+              clear_buffers(buffer,command);
+              main_menu(sfd,buffer,command,is_authenticated);
+            }
+          }
 
           snprintf(command,256, "ADD %s %s", nome_contatto,numero_contatto);
           send(sfd, command, strlen(command), 0);
@@ -64,7 +140,7 @@ void main_menu(int sfd,char *buffer,char *command,int is_authenticated){
 
     case 2:
           if(is_authenticated==0){
-          printf("You must be logged-in\n");
+          print_error("You must be logged-in\n");
           main_menu(sfd,buffer,command,is_authenticated);
           }
           clear_buffers(buffer,command);
@@ -112,8 +188,9 @@ void main_menu(int sfd,char *buffer,char *command,int is_authenticated){
          exit(EXIT_SUCCESS);
 
     default:
-          printf("Opzione non valida\n");
-          break;
+          print_error("Opzione non valida\n");
+          clear_buffers(buffer,command);
+          main_menu(sfd,buffer,command,is_authenticated);
          }
       }
    }
@@ -147,11 +224,24 @@ void login(int sfd){
   int is_authenticated=0;
   char buffer[200];
   int choice;
+  char line[100];
   char command[256];
+  clear_buffers(buffer,command);
 
   gui();
-  scanf("%d",&choice);
-  getchar();
+  if(fgets(line,sizeof(line),stdin)!=NULL){
+    line[strcspn(line,"\n")]=0;
+    if(strlen(line)==1 && isdigit(line[0])){
+      choice=line[0]-'0';
+    }
+    else {
+    print_error("input non valido\n");
+    clear_buffers(buffer,command);
+    memset(line, 0, sizeof(line));
+    login(sfd);
+    }
+  }
+
   switch(choice){
 
   case 1:
@@ -159,6 +249,11 @@ void login(int sfd){
     printf("Inserisci utente: ");
     fgets(username, sizeof(username), stdin);
     username[strcspn(username, "\n")] = 0;
+    if(!validate_username(username)){
+      print_error("Username non valido. Deve essere lungo tra 3 e 20 caratteri e contenere solo lettere, numeri e underscore.\n");
+      clear_buffers(buffer,command);
+      login(sfd);
+    }
     char password[50];
     printf("Inserisci password: ");
     fgets(password,sizeof(password),stdin);
@@ -166,9 +261,9 @@ void login(int sfd){
     snprintf(command, sizeof(command), "LOGIN %s %s", username,password);
     send(sfd, command, strlen(command), 0);
     recv(sfd, buffer, sizeof(buffer) - 1, 0);
-    printf("%s",buffer);
 
     if(strncmp(buffer,"LOGIN effettuato",16)==0){
+      print_success("LOGIN effettuato");
       is_authenticated=1;
       clear_buffers(buffer,command);
       main_menu(sfd,buffer,command,is_authenticated);
@@ -176,9 +271,9 @@ void login(int sfd){
     }
     else{
       buffer[255] = '\0';
-      printf("\n%s\n", buffer);
+      print_error("Nome utente o password errato\n");
       clear_buffers(buffer,command);
-      break;
+      login(sfd);
   }
 
   case 2:
@@ -186,25 +281,41 @@ void login(int sfd){
   printf("Inserisci utente: ");
   fgets(new_username, sizeof(new_username), stdin);
   new_username[strcspn(new_username, "\n")] = 0;
+  if(validate_username(new_username)==0){
+    print_error("Username non valido. Deve essere lungo tra 3 e 20 caratteri e contenere solo lettere, numeri e underscore.\n");
+    clear_buffers(buffer,command);
+    login(sfd);
+  }
+
   char new_password[50];
   printf("Inserisci password: ");
   fgets(new_password,sizeof(new_password),stdin);
   new_password[strcspn(new_password,"\n")]=0;
+  if (!validate_password(new_password)) {
+    print_error("Password non valida. Deve essere lunga tra 8 e 30 caratteri e contenere almeno una lettera maiuscola, una minuscola, un numero e un carattere speciale.\n");
+    login(sfd);
+  }
+
   snprintf(command, sizeof(command), "SIGNUP %s %s", new_username,new_password);
   send(sfd, command, strlen(command), 0);
   ssize_t recv_len=recv(sfd, buffer, sizeof(buffer) - 1, 0);
   buffer[recv_len]='\0';
-  printf("%s\n",buffer);
   if(strncmp(buffer,"SIGN UP riuscito",16)==0){
+    print_success("SIGN UP riuscito\n");
     is_authenticated=1;
     clear_buffers(buffer,command);
     main_menu(sfd,buffer,command,is_authenticated);
+  }
+  else{
+    print_error("Utente già registrato\n");
+    clear_buffers(buffer,command);
+    login(sfd);
   }
 
   case 3:
     send(sfd,"NOACC ",6,0);
     recv(sfd,buffer,sizeof(buffer)-1,0);
-    printf("%s",buffer);
+    print_warning("!Warning! Se non esegui l'accesso non potrai aggiungere/rimuovere contatti\n");
     clear_buffers(buffer,command);
     main_menu(sfd,buffer,command,is_authenticated);
 
@@ -220,7 +331,7 @@ void server_login(int sfd){
   while(1){
   int cfd=accept(sfd,NULL,NULL);
   if(cfd==-1){
-    printf("error accepting connetion\n");
+    print_error("error accepting connetion\n");
     continue;
   }
   printf("client conneted\n");
@@ -303,43 +414,54 @@ void server_login(int sfd){
     send(cfd,"Contatto aggiunto",17,0);
   }
   else if(strncmp(buffer,"DELETE ",7)==0){
-    FILE *f1=fopen("Contatti.txt","r");
-    FILE *f2=fopen("temp.txt","w");
-    if(f1==NULL||f2==NULL){
-      printf("error opening file\n");
-      exit(EXIT_FAILURE);
-    }
-    char nome_ricevuto[50];
-    int found=0;
-    char riga[256];
+  FILE *f1=fopen("Contatti.txt","r");
+  FILE *f2=fopen("temp.txt","w");
+  if(f1==NULL||f2==NULL){
+    perror("Error opening file");
+    send(cfd,"Errore nell'eliminazione del contatto\n",39,0);
+    if(f1) fclose(f1);
+    if(f2) fclose(f2);
+    return;
+  }
+  char nome_ricevuto[50];
+  int found=0;
+  char riga[256];
 
-    sscanf(buffer+7,"%s",nome_ricevuto);
-    while(fgets(riga,256,f1)!=NULL){
-      char nome_nel_file[50];
-      char numero_nel_file[50];
-      sscanf(riga,"%s %s",nome_nel_file,numero_nel_file);
-      printf("%s\n",nome_nel_file);
-      printf("%s\n",nome_ricevuto);
-      if(strcmp(nome_nel_file,nome_ricevuto)==0){
+  sscanf(buffer+7,"%49s",nome_ricevuto);
+  while(fgets(riga,sizeof(riga),f1)){
+    char nome_nel_file[50];
+    char numero_nel_file[50];
+    if(sscanf(riga,"%49s %49s",nome_nel_file,numero_nel_file) == 2){
+      if(strcmp(nome_nel_file,nome_ricevuto)!=0){
+        fprintf(f2,"%s %s\n",nome_nel_file,numero_nel_file);
+      } else {
         found=1;
       }
-      else{
-        fprintf(f2,"%s %s\n",nome_nel_file,numero_nel_file);
-      }
-    }
-    fclose(f1);
-    fclose(f2);
-
-    if(found==0){
-      remove("temp.txt");
-      send(cfd,"Nome utente non trovato\n",25,0);
-    }
-    else{
-      remove("Contatti.txt");
-      rename("temp.txt","Contatti.txt");
-      send(cfd,"Contatto eliminato\n",20,0);
     }
   }
+  fflush(f2);
+  fclose(f1);
+  fclose(f2);
+
+  if(found==0){
+    remove("temp.txt");
+    send(cfd,"Nome utente non trovato\n",25,0);
+  }
+  else{
+    if(remove("Contatti.txt") != 0){
+      perror("Error deleting original file");
+      remove("temp.txt");
+      send(cfd,"Errore nell'eliminazione del contatto\n",39,0);
+      return;
+    }
+    if(rename("temp.txt","Contatti.txt") != 0){
+      perror("Error renaming file");
+      send(cfd,"Errore nell'eliminazione del contatto\n",39,0);
+      return;
+    }
+    send(cfd,"Contatto eliminato\n",20,0);
+  }
+}
   else if(strncmp(buffer,"VIEW",4)==0){
     FILE *f1 = fopen("Contatti.txt", "r");
     if (f1== NULL) {
@@ -347,16 +469,19 @@ void server_login(int sfd){
         send(cfd, "Errore nel recupero dei contatti\n", 34, 0);
         return;
         }
-    char rubrica[10000];
-    char riga[256]="Contatti:\n\n";
+    char rubrica[10000] = "Contatti:\n\n";
+    char riga[256];
     while(fgets(riga,sizeof(riga),f1)!=NULL){
-      strcat(buffer,riga);
+      char nome[50], numero[50];
+      if(sscanf(riga,"%s %s",nome,numero) == 2){
+        strcat(rubrica, riga);
+      }
     }
     fclose(f1);
-    if(strlen(rubrica)==strlen(riga)){
-      strcat(buffer,"Nessun contatto trovato\n");
+    if(strlen(rubrica)==strlen("Contatti:\n\n")){
+      strcat(rubrica,"Nessun contatto trovato\n");
     }
-    send(cfd,buffer+4,sizeof(buffer)-4,0);
+    send(cfd,rubrica,strlen(rubrica),0);
       }
     }
   }
--- progetto_rubrica/utility.h
+++ progetto_rubrica/utility.h
@@ -9,5 +9,10 @@ void login(int sfd);
 void server_login(int sfd);
 void main_menu(int sfd,char *buffer,char *command,int is_authenticated);
 void clear_buffers(char *buffer,char *command);
+int validate_username(char *username);
+int validate_password(char *password);
+void print_error(const char *messaggio);
+void print_warning(const char* message);
+void print_success(const char* message);
 
 #endif
